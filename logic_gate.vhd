cf500cbc28558442f19eb88ec82d45eb1eaa9f37
Isac E Perez <89553126+Isac-E-Perez@users.noreply.github.com>
2021-10-09 17:36:05 -0500

    Update logic_gate.vhd

--- logic_gate.vhd
+++ logic_gate.vhd
@@ -1,5 +1,5 @@
 library ieee;
-use ieee.std_logic_116.all; -- Though different standards are available, a common one is the IEEE std_logic_1164 that allos fro the basic types we are using 
+use ieee.std_logic_116.all; -- Though different standards are available, a common one is the IEEE std_logic_1164 that allows for the basic types we are using 
 
 entity logic_gate is 
   port ( A, B, C: in std_logic;
